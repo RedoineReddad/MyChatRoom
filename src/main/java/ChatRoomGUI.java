5f4f3bfeb7dd4e19291c6aee4753f247a87f928a
jean-baptiste <vaiagina@et.esiea.fr>
2020-05-30 08:28:47 +1100

    tu peux vérifier2

--- src/main/java/ChatRoomGUI.java
+++ src/main/java/ChatRoomGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatRoomGUI extends JFrame{
     private JPanel mainPanel;
@@ -40,6 +42,127 @@ public class ChatRoomGUI extends JFrame{
         frame.pack();
         frame.setVisible(true);
 
+        redoineButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String redoineBottonText = redoineButton.getText();
+                searchPanel.setText(redoineBottonText);
+            }
+        });
+
+        julienButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String julienBottonText = julieButton.getText();
+                searchPanel.setText(julienBottonText);
+            }
+        });
+
+        julieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String julieBottonText = julieButton.getText();
+                searchPanel.setText(julieBottonText);
+            }
+        });
+
+        antoineButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String antoineBottonText = antoineButton.getText();
+                searchPanel.setText(antoineBottonText);
+            }
+        });
+
+        martinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String martinBottonText = martinButton.getText();
+                searchPanel.setText(martinBottonText);
+            }
+        });
+
+        filippoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String filippoBottonText = filippoButton.getText();
+                searchPanel.setText(filippoBottonText);
+            }
+        });
+
+        valérianButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String valérianBottonText = valérianButton.getText();
+                searchPanel.setText(valérianBottonText);
+            }
+        });
+
+        jingyangButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String jingyangBottonText = jingyangButton.getText();
+                searchPanel.setText(jingyangBottonText);
+            }
+        });
+
+        lolaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String lolaBottonText = lolaButton.getText();
+                searchPanel.setText(lolaBottonText);
+            }
+        });
+
+        angeloButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String angeloBottonText = angeloButton.getText();
+                searchPanel.setText(angeloBottonText);
+            }
+        });
+
+        marieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String marieBottonText = marieButton.getText();
+                searchPanel.setText(marieBottonText);
+            }
+        });
+
+        fredButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fredBottonText = fredButton.getText();
+                searchPanel.setText(fredBottonText);
+            }
+        });
+
+        jamyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String jamyBottonText = jamyButton.getText();
+                searchPanel.setText(jamyBottonText);
+            }
+        });
+
+        davidButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String davidBottonText = davidButton.getText();
+                searchPanel.setText(davidBottonText);
+            }
+        });
+
+        eliseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String eliseBottonText = eliseButton.getText();
+                searchPanel.setText(eliseBottonText);
+            }
+        });
+
+
     }
 
     public static void main(String[] args){
@@ -57,12 +180,11 @@ public class ChatRoomGUI extends JFrame{
         angeloButton = new JButton(new ImageIcon("perso.png"));
         antoineButton = new JButton(new ImageIcon("perso.png"));
         redoineButton = new JButton(new ImageIcon("perso.png"));
-        jamyButton = new JButton(new ImageIcon("perso.png"));
+        davidButton = new JButton(new ImageIcon("perso.png"));
         marieButton = new JButton(new ImageIcon("perso.png"));
         julienButton = new JButton(new ImageIcon("perso.png"));
         eliseButton = new JButton(new ImageIcon("perso.png"));
         fredButton = new JButton(new ImageIcon("perso.png"));
         jamyButton = new JButton(new ImageIcon("perso.png"));
-
     }
 }
